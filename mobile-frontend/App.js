189dcff8c3e455166bc477154b7b9b35862958bc
it21042560 <it21042560@my.sliit.lk>
2023-10-15 21:11:05 +0530

    smart card

--- mobile-frontend/App.js
+++ mobile-frontend/App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import SmartCard from './screen/Smartcard';
 
-export default function App() {
+const Stack = createStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
+
+function MainStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name='NutritionPlanSelect' component={SmartCard} options={{ headerShown: false }} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer> 
+    <Tab.Navigator barStyle={{backgroundColor:'#4169e1'}}>
+      <Tab.Screen
+        name="Home"
+        component={MainStack}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name={focused ? 'ios-home' : 'ios-home-outline'} size={size} color={color} />
+          ),
+          headerShown:false
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={MainStack}
+        options={{
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name={focused ? 'ios-settings' : 'ios-settings-outline'} size={size} color={color} />
+          ),
+          headerShown:false
+        }}
+      />
+    </Tab.Navigator>
+  </NavigationContainer>
+  );
+}
+
